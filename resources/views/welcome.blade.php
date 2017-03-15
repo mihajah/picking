72094a4704cfa8a4c38b43b54e9cc0d7dbf3fe82
TSIRY HERY Ny Aina Pappy <ainapappy@gmail.com>
2017-03-15 19:20:18 +0300

    Update welcome.blade.php
    fix error commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,6 @@
 		<script src="{{asset('public/js/ajax.js')}}"></script>
 		<script src="{{asset('public/js/config.js')}}"></script> 
 	</head>
-	
 	<body class="picking">
 		<!-- debut menu -->
 		<div id="tcz_header_commun_menu"/>
